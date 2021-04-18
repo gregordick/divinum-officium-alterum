cd0c5676b4a79f0034ae8d46e334d6013f0cc777
Gregor Dick <igregord@gmail.com>
2021-04-18 23:19:50 +0100

    Divisions of psalms.

--- src/officium/parts.py
+++ src/officium/parts.py
@@ -1,4 +1,5 @@
 import itertools
+import re
 
 from officium import data
 from officium.psalmish import descriptor_to_psalmish
@@ -31,18 +32,26 @@ class StructuredLookup:
     labelled_classes = {
     }
 
-    def __init__(self, path, default_class=Group, list_root=False):
+    def __init__(self, path, default_class=Group, list_root=False,
+                 raw_filter=None):
         self.path = path
         self.default_class = default_class
         self.list_root = list_root
+        self.raw_filter = raw_filter
 
     def __repr__(self):
         return '%s(%s, %s)' % (self.__class__.__name__, self.path,
                                self.default_class.__name__)
 
+    def lookup_raw(self, lang_data):
+        raw = lang_data.get(self.path, self.path)
+        if self.raw_filter:
+            raw = self.raw_filter(raw)
+        return raw
+
     def resolve(self, lang_data):
         # XXX: Silently handle missing things by returning the raw path.
-        child = lang_data.get(self.path, self.path)
+        child = self.lookup_raw(lang_data)
         if self.list_root:
             classes = itertools.cycle([self.default_class])
             return self.build_renderable_list(classes, child)
@@ -83,11 +92,39 @@ class PsalmishWithAntiphon:
         self.antiphon = antiphon
         self.psalmishes = psalmishes
 
+    @staticmethod
+    def make_psalm_verse_filter(start, end):
+        def the_filter(raw_verses):
+            def gen():
+                for verse in raw_verses:
+                    # XXX: The verse-numbering should be specified in a more
+                    # structured way, rather than in line with the text.
+                    m = re.match(r'\d+:(\d+)', verse)
+                    if not m:
+                        # XXX: Shouldn't happen, but we can't guarantee this
+                        # yet.
+                        yield verse
+                    else:
+                        verse_num = int(m.group(1))
+                        if start <= verse_num <= end:
+                            yield verse
+            # XXX: It would be nice not to have to collapse the generator here.
+            return list(gen())
+        return the_filter
+
     def resolve(self):
         # XXX: Proper classes, Gloria.
         yield self.antiphon
         for psalmish in self.psalmishes:
-            yield StructuredLookup(psalmish.key, PsalmVerse)
+            # XXX: This is a DO-style partial-psalm spec.
+            m = re.match(r'(.*)[(](\d+)-(\d+)[)]', psalmish.key)
+            if m:
+                key, start, end = m.groups()
+                the_filter = self.make_psalm_verse_filter(int(start), int(end))
+            else:
+                key = psalmish.key
+                the_filter = None
+            yield StructuredLookup(key, PsalmVerse, raw_filter=the_filter)
             if psalmish.gloria:
                 yield StructuredLookup('versiculi/gloria-patri-post-psalmum',
                                        PsalmVerse, list_root=True)
