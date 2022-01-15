gregordick/divinum-officium-alterum

commit e18fb7437404e8fcdf6811879a7282a5fdae68c8

Use -p variants for commons in Paschaltide.

src/officium/vespers.py
@@ -33,17 +33,18 @@ class Vespers:
         else:
             self._doxology = None
 
-    def lookup_order(self, office, items):
-        # XXX: The use of lambdas here is probably overengineered -- we could
-        # just mandate that the paths are prefixes and do a join rather than a
-        # call -- and the scoping doesn't behave in the way that we'd like.
+    def lookup_order(self, office, items, use_commons=True):
+        paths = []
 
-        paths = [
-            lambda item, key=key: '%s/%s' % (key, item)
-            for key in office.keys
-        ]
+        for keys in [office.keys] + ([['%s/commune' % (key,)
+                                       for key in office.keys]]
+                                     if use_commons else []):
+            if self._season:
+                paths += ['%s/%s' % (key, self._season) for key in keys]
+            paths += list(keys)
+
         season = [
-            lambda item, key=key: 'proprium/%s/%s' % (key, item)
+            'proprium/%s' % (key,)
             for key in self._season_keys
         ]
         # Seasonal keys take precedence over office keys for and only for
@@ -52,28 +53,30 @@ class Vespers:
             paths = season + paths
         else:
             paths += season
+
         # Fall back to propers from the preceding Sunday.  XXX: This is wrong,
         # in two respects: firstly, it should be any non-Sunday temporal
         # office, and not necessarily a feria; and secondly, slicing and dicing
         # the first key is in very poor taste.
         if isinstance(office, offices.Feria):
             sunday = office.keys[0][:-1] + '0'
-            paths.append(lambda item: 'proprium/%s/%s' % (sunday, item))
-        prefixes = []
+            paths.append('proprium/%s' % (sunday,))
+
+        psalter_prefixes = []
         if self._season:
-            prefixes.append(self._season + '/')
-        prefixes.append('')
-        for prefix in prefixes:
+            psalter_prefixes.append('/' + self._season)
+        psalter_prefixes.append('/')
+        for prefix in psalter_prefixes:
             paths += [
-                lambda item, prefix=prefix: 'psalterium/%s%s/%s' % (prefix,
-                                                                    util.day_ids[self._date.day_of_week],
-                                                                    item),
-                lambda item, prefix=prefix: 'psalterium/%s%s' % (prefix, item),
+                'psalterium%s/%s' % (prefix,
+                                     util.day_ids[self._date.day_of_week],),
+                'psalterium%s' % (prefix,),
             ]
+
         items = list(items)
         for path in paths:
             for item in items:
-                yield path(item)
+                yield '/'.join([path, item])
 
     def lookup(self, office, is_first, *items):
         base = [

scripts/bringup/bringup-datagen.py
@@ -595,7 +595,12 @@ def merge_do_section(propers, redirections, do_redirections, generic, options,
             assert out_key_base
             basename = m.group(2)
             if re.match(r'C\d+', basename):
-                redirections[out_key_base] = 'commune/' + basename
+                out_key_common = make_full_path(out_key_base, 'commune')
+                redirections[out_key_common] = 'commune/' + basename
+                # Use the -p variant for Easter.  These don't always exist, but
+                # those cases will get dropped cleanly in due course.
+                easter_common_out_key = make_full_path(out_key_common, 'pasc')
+                redirections[easter_common_out_key] = 'commune/%sp' % (basename,)
             else:
                 if '/' not in basename:
                     if re.match(r'\d\d-\d\d', basename):

commit e374cc8aac83d1c0d0add700ef186e7b689d40f1

Fix regression in lookups in psalter.
Fixes: e18fb7437404e8fcdf6811879a7282a5fdae68c8

src/officium/vespers.py
@@ -65,7 +65,7 @@ class Vespers:
         psalter_prefixes = []
         if self._season:
             psalter_prefixes.append('/' + self._season)
-        psalter_prefixes.append('/')
+        psalter_prefixes.append('')
         for prefix in psalter_prefixes:
             paths += [
                 'psalterium%s/%s' % (prefix,